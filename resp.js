71c6d5eb7b2aeb63ad23a40e3192a7ecf385b8f2
hashirtariq <95280315+hashirtariq@users.noreply.github.com>
2021-11-30 14:53:40 +0500

    Project

---
+++ resp.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector(".navbar");
+const ni = document.querySelector(".ni");
+const bs = document.querySelector(".rightnav");
+const burger = document.querySelector(".burger");
+function myfun() {
+  bs.classList.toggle("resp2");
+  ni.classList.toggle("resp");
+  navbar.classList.toggle("resp3");
+}
